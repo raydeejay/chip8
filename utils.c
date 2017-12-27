0582b82468d16a23889722c3a7135aa043132d5b
Sergi Reyner <sergi.reyner@gmail.com>
2017-12-27 05:08:23 +0100

    Moved utility code to utils.c/utils.h

---
+++ utils.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *rtrim(const char *s)
+{
+    while( isspace(*s) || !isprint(*s) ) ++s;
+    return strdup(s);
+}
+
+char *ltrim(const char *s)
+{
+    char *r = strdup(s);
+    if (r != NULL)
+    {
+        char *fr = r + strlen(s) - 1;
+        while( (isspace(*fr) || !isprint(*fr) || *fr == 0) && fr >= r) --fr;
+        *++fr = 0;
+    }
+    return r;
+}
+
+char *trim(const char *s)
+{
+    char *r = rtrim(s);
+    char *f = ltrim(r);
+    free(r);
+    return f;
+}
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+extern char *rtrim(const char *s);
+extern char *ltrim(const char *s);
+extern char *trim(const char *s);
+#endif
